c57281f27b96257f8f750b0f68fcaf599be6db2f
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-04-09 12:50:29 +0300

    Create gpa.py as the main module

---
+++ gpa/gpa.py
@@ -0,0 +1,14 @@
+# *-* encoding=utf-8 *-*
+
+import getpass
+import re
+import requests
+import sys
+
+from adapter import TLSv1Adapter
+from ects import ECTSRule
+from student import StudentRecord
+
+
+if __name__ == '__main__':
+    pass
